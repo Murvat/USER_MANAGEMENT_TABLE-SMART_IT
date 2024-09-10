e877b2e6070d7b62ed0953d0534b4b481de1ac9c
Murvat <murvat.mustafayev@mail.ru>
2024-09-10 21:11:07 +0100

    Project_Start

---
+++ USER_MANAGEMENT_TABLE/src/App.tsx
@@ -0,0 +1,59 @@
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+      <form action="">
+        <label htmlFor=""></label>
+        <input type="text" />
+        <input type="number" />
+        <button>Search</button>
+      </form>
+  <table id="example" className="table table-striped">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011-04-25</td>
+                <td>$320,800</td>
+            </tr>
+          
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Age</th>
+                <th>Start date</th>
+                <th>Salary</th>
+            </tr>
+        </tfoot>
+      </table>
+      <nav aria-label="Page navigation example">
+  <ul  className="pagination">
+    <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+    <li className="page-item"><a className="page-link" href="#">1</a></li>
+    <li className="page-item"><a className="page-link" href="#">2</a></li>
+    <li className="page-item"><a className="page-link" href="#">3</a></li>
+    <li className="page-item"><a className="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+    </>
+  )
+}
+
+export default App
